d53c42608b216487f754e4f40af2cb6e1e9c676a
boka <beni.r@panorays.com>
2022-02-16 01:00:06 +0200

    test: wip

--- cypress/integration/login/login.spec.ts
+++ cypress/integration/login/login.spec.ts
@@ -1,3 +1,6 @@
+let count;
+let googleCount = [];
+
 describe('login', () => {
   it('should be able to login', () => {
     cy.visit('https://www.facebook.com/');
@@ -8,13 +11,32 @@ describe('login', () => {
   });
 
   it.only('orders', () => {
-    cy.fixture('test.json').each(el => {
+    let times;
+    let check;
+    cy.fixture('test.json').then(all => (times = all.length));
+
+    cy.fixture('test.json').each((el, index) => {
       const url = `${el.website}`;
       cy.log(url);
       cy.visit('https://www.google.com/search', { qs: { q: url } }).waitForResources();
       cy.get('#result-stats').invoke('text').as('result');
-      cy.get('@result').then(str => cy.log(str.split('תוצאות')[0].replace(/\D/g, '')));
-      cy.pause();
+
+      cy.get('@result').then(result => {
+        const test = result.find('#taw');
+        cy.log(test);
+        // if (result.find('#taw')) check = !!result.find('#taw');
+        debugger;
+      });
+
+      // cy.get('@result').then(str => (count = str.split('תוצאות')[0].replace(/\D/g, '')));
+      // el.google_count = check ? count : 0;
+      // googleCount.push(el);
+      // cy.log(JSON.stringify(googleCount));
+      // times--;
+      // cy.log(`times - ${times}`);
+      // cy.pause();
+
+      // debugger;
     });
     // cy.writeFile(`cypress/fixtures/googleSum.json`, googleSum);
   });
