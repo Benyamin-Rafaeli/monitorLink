3b284f0c0cfb031f49ba49173ddf5273cacb5bea
boka <beni.r@panorays.com>
2022-02-17 14:24:18 +0200

    test: added table

---
+++ cypress/fixtures/index.js
@@ -0,0 +1,26 @@
+fetch('./test.json')
+  .then(data => {
+    return data.json();
+  })
+  .then(readyData => {
+    const container = document.getElementById('example');
+    // const columns = ['hashtag', 'website', 'linkedinCounter', 'tralivali'];
+    const columns = Object.keys(readyData[0]);
+
+    const hot = new Handsontable(container, {
+      data: readyData,
+      rowHeaders: true,
+      colHeaders: columns,
+      height: 'auto',
+      licenseKey: 'non-commercial-and-evaluation',
+      columns: columns.map(column => ({ data: column, editor: false })),
+      columnSorting: {
+        sortEmptyCells: true,
+        initialConfig: {
+          column: 2,
+          sortOrder: 'asc',
+        },
+      },
+    });
+    // console.log(readyData);
+  });
