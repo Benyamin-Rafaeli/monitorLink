9637ec9bc98b00d609251a32faf2113bca6a50ee
boka <beni.r@panorays.com>
2022-02-19 05:11:02 +0200

    test: done google

--- cypress/integration/login/login.spec.ts
+++ cypress/integration/login/login.spec.ts
@@ -1,6 +1,14 @@
 // a.map(el => Object.assign({linkedin: '', }, el))
 
 let graph = [];
+let count;
+let total;
+
+const forceControl = (index: number) => {
+  if (Number(total - index) % 10 === 0) {
+    cy.pause();
+  }
+};
 
 describe('login', () => {
   before(() => {
@@ -9,15 +17,10 @@ describe('login', () => {
     cy.loginUi(username, password).waitForResources();
   });
 
-  it('count all general', () => {
-    let count;
-    let total;
+  it.skip('count all general', () => {
     cy.fixture('test').then(array => {
-      count = array.length;
       total = array.length;
-
       array.forEach((item, index) => {
-        cy.log(`count : ${count}`);
         cy.log(`total : ${total - index}`);
 
         cy.navigate(undefined, `#${item.hashtag}`);
@@ -27,47 +30,34 @@ describe('login', () => {
           .then(time => {
             graph.push({ website: item.website, hashtag: item.hashtag, linkedin: String(time).trim() });
             cy.log(JSON.stringify(graph));
-            cy.log(String(count));
-            count -= 1;
           })
           .then(() => cy.writeFile(`cypress/fixtures/graph.json`, graph));
 
-        if (Number(total - index) % 10 === 0) {
-          cy.pause();
-        }
+        forceControl(index);
       });
     });
   });
 
-  it.skip('gulugulu', () => {
-    let times;
-    let check;
-    cy.fixture('1_test.json').then(all => (times = all.length));
+  it.skip('google links counts', () => {
+    cy.fixture('2_test').then(array => {
+      total = array.length;
+      array.forEach((item, index) => {
+        cy.log(`total : ${total - index}`);
 
-    cy.fixture('1_test.json').each((el, index) => {
-      const url = `${el.website}`;
-      cy.log(url);
-      cy.visit('https://www.google.com/search', { qs: { q: url } }).waitForResources();
-      cy.get('#result-stats').invoke('text').as('result');
+        cy.visit('https://www.google.com/search', { qs: { q: item.website } }).waitForResources();
+        cy.get('#result-stats').invoke('text').as('result');
+        cy.get('@result')
+          .then(result => {
+            // @ts-ignore
+            item.googleCount = result.split('תוצאות')[0].replace(/^\D+/g, '').trim();
+            graph.push(item);
+            cy.log(JSON.stringify(graph));
+          })
+          .then(() => cy.writeFile(`cypress/fixtures/graph.json`, graph));
 
-      cy.get('@result').then(result => {
-        const test = result.find('#taw');
-        cy.log(test);
-        // if (result.find('#taw')) check = !!result.find('#taw');
-        debugger;
+        forceControl(index);
       });
-
-      // cy.get('@result').then(str => (count = str.split('תוצאות')[0].replace(/\D/g, '')));
-      // el.google_count = check ? count : 0;
-      // googleCount.push(el);
-      // cy.log(JSON.stringify(googleCount));
-      // times--;
-      // cy.log(`times - ${times}`);
-      // cy.pause();
-
-      // debugger;
     });
-    // cy.writeFile(`cypress/fixtures/googleSum.json`, googleSum);
   });
 
   it.skip('should be able to login', () => {
