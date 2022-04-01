02f395d731b5d0a5184964a2fd217b523b8aceec
boka <beni.r@panorays.com>
2022-04-01 23:52:26 +0300

    test: wip

--- cypress/integration/login/collect.spec.ts
+++ cypress/integration/login/collect.spec.ts
@@ -209,4 +209,24 @@ describe('collect', () => {
     //   else cy.log('my sweet sweet results', results);
     // });
   });
+
+  let myName = 'HolyToledo';
+  let myPath = 'C:\\Users\\someSecretPath\\assets\\devArticles';
+
+  it.skip('scrap - get all my articles', () => {
+    // get all of my dev.to articles
+    cy.request(`https://dev.to/api/articles?username=${myName}&per_page=2000`).then(result => {
+      let titles = result.body.map(item => {
+        // put all articles into an array of objects with title and url
+        let result = { title: item.title, url: item.url };
+        return result;
+      });
+      let sorted = titles.sort();
+      // make it suitable for a json file.
+      let object = { Articles: sorted };
+      let path = `${myPath}\\articles.json`;
+      // write it to my assets folder for the web display.
+      cy.writeFile(path, object);
+    });
+  });
 });
